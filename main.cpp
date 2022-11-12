02b4bdc06ce0e181cba2e1b580c1cabc35c74850
benjacd <bcastro@utem.cl>
2022-11-11 22:24:17 -0300

    Paralelo?

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
-#include <omp.h>
-int main() {
-using namespace std;
+#include <iomanip>
+#include <chrono>
 
-#pragma omp parallel
+int main()
 {
-int id = omp_get_thread_num(); 
-cout << "Hola desde el hilo {" << id << "}!" << endl;
-}
+    using namespace std;
+    using namespace std::chrono;
 
-return 0;
+    constexpr long nsteps = 10000000;
+    double step = 1.0 / double(nsteps);
+
+    using clk = high_resolution_clock;
+    auto t1 = clk ::now();
+    double sum = 0.0;
+#pragma omp parallel
+    {
+        for (int i = 0; i < nsteps; ++i)
+        {
+            double x = (i + 0.5) * step;
+            sum += 4.0 / (1.0 + x * x);
+        }
+    }
+    auto t2 = clk ::now();
+    double pi = step * sum;
+    auto diff = duration_cast<microseconds>(t2 - t1);
+
+    cout << "pi =" << setprecision(10) << pi << endl;
+    cout << "tiempo = " << diff.count() << " microsegundos" << endl;
+
+    return 0;
 }
