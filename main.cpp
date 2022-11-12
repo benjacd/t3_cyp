3c6621c0ace5749a694215f698944302b165d078
benjacd <bcastro@utem.cl>
2022-11-11 22:53:28 -0300

    finalizado

--- main.cpp
+++ main.cpp
@@ -6,21 +6,20 @@ int main()
 {
     using namespace std;
     using namespace std::chrono;
-
-    constexpr long nsteps = 10000000;
+    // Se modifico la cantidad de steps para contrastar los tiempos secuencial v/s paralelo
+    constexpr long nsteps = 1'000'000'000;
     double step = 1.0 / double(nsteps);
 
     using clk = high_resolution_clock;
     auto t1 = clk ::now();
     double sum = 0.0;
-#pragma omp parallel
-    {
+   #pragma omp parallel for reduction(+:sum)
         for (int i = 0; i < nsteps; ++i)
         {
             double x = (i + 0.5) * step;
-            sum += 4.0 / (1.0 + x * x);
+            sum += 4.0 / (1.0 + (x * x));
         }
-    }
+
     auto t2 = clk ::now();
     double pi = step * sum;
     auto diff = duration_cast<microseconds>(t2 - t1);
